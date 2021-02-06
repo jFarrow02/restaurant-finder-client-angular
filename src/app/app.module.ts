b58a81b6703f421e120cc8d5c8321126baa74902
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-02-06 11:54:39 -0500

    creates restaurant list dir structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantListComponent
   ],
   imports: [
     BrowserModule,
